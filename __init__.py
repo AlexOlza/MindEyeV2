5f2705ad8f6568fd973f2bef4a0361f994c77a3b
AlexOlza <alexander.olza@ehu.eus>
2025-07-08 16:55:22 +0200

    Replace deprecated cached_download with hf_hub_download

---
+++ __init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  8 15:03:49 2025
+
+@author: alexolza
+"""
+
---
+++ src/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  8 15:04:14 2025
+
+@author: alexolza
+"""
+
